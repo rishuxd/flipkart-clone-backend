adbe0e88e5c25be80893a95ff6e16353948a03a4
Jyotiswaroop Srivastav <srivastavjyotiswaroop@gmail.com>
2023-02-20 23:08:30 +0530

    login API for existing user

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,3 +1,4 @@
+import { response } from "express";
 import User from "../model/userSchema.js";
 
 export const userSignup = async (request, response) => {
@@ -16,4 +17,18 @@ export const userSignup = async (request, response) => {
   }
 };
 
-export default userSignup;
+export const userLogin = async (request, response) => {
+  try {
+    const username = request.body.username;
+    const password = request.body.password;
+
+    let user = await User.findOne({ username: username, password: password });
+    if (user) {
+      return response.status(200).json({ data: user });
+    } else {
+      return response.status(401).json("Invalid login");
+    }
+  } catch (error) {
+    response.status(500).json("Error", error.message);
+  }
+};
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import userSignup from "../controller/user-controller.js";
+import { userSignup, userLogin } from "../controller/user-controller.js";
 
 const Router = express.Router();
 
 Router.post("/signup", userSignup);
+Router.post("/login", userLogin);
 
 export default Router;
