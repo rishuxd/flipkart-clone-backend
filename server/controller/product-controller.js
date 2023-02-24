52620ad05a7f42db51702387416438e637db8572
Jyotiswaroop Srivastav <srivastavjyotiswaroop@gmail.com>
2023-02-24 22:35:13 +0530

    products API

---
+++ server/controller/product-controller.js
@@ -0,0 +1,11 @@
+import { response } from "express";
+import Product from "../model/productSchema.js";
+
+export const getProducts = async (request, response) => {
+  try {
+    const products = await Product.find({});
+    response.status(200).json(products);
+  } catch (error) {
+    response.status(500).json({ message: error.message });
+  }
+};
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { userSignup, userLogin } from "../controller/user-controller.js";
+import { getProducts } from "../controller/product-controller.js";
 
 const Router = express.Router();
 
 Router.post("/signup", userSignup);
 Router.post("/login", userLogin);
+Router.get("/products", getProducts);
 
 export default Router;
