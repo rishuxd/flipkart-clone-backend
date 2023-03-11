347753bb420acfdf628c547d907396b0abf47920
Jyotiswaroop Srivastav <srivastavjyotiswaroop@gmail.com>
2023-03-11 23:59:52 +0530

    Product Detail API

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { userSignup, userLogin } from "../controller/user-controller.js";
-import { getProducts } from "../controller/product-controller.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controller/product-controller.js";
 
 const Router = express.Router();
 
 Router.post("/signup", userSignup);
 Router.post("/login", userLogin);
 Router.get("/products", getProducts);
+Router.get("/product/:id, getProductById");
 
 export default Router;
